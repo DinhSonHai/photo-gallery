554c9f0334ac61320b5b701caae63e41901d157f
DinhSonHai <sdinhhai@gmail.com>
2020-10-01 21:14:15 +0700

    Create useFirestore hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ImageGrid from './components/ImageGrid';
 
 import Title from './components/Title';
 import UploadForm from './components/UploadForm';
@@ -8,6 +9,7 @@ function App() {
     <div className='App'>
       <Title />
       <UploadForm />
+      <ImageGrid />
     </div>
   );
 }
---
+++ src/components/ImageGrid.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ImageGrid = () => {
+  return <div className='iamge-grid'></div>;
+};
+
+export default ImageGrid;
