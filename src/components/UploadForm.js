4adf7ad0362f18c6e37b48c926391f27a18c511b
DinhSonHai <sdinhhai@gmail.com>
2020-10-01 10:05:30 +0700

    Create UploadForm component

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UploadForm = () => {
-  return <div className='upload-form'></div>;
+  const [file, setFile] = useState(null);
+  const [fileUrl, setFileUrl] = useState(null);
+  const [error, setError] = useState('');
+
+  const types = ['image/png', 'image/jpeg'];
+
+  const changeHandler = (e) => {
+    let selected = e.target.files[0];
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setFileUrl(URL.createObjectURL(selected));
+      setError('');
+    } else {
+      setFile(null);
+      setFileUrl(null);
+      setError('Please select valid image file (png or jpeg)');
+    }
+  };
+  return (
+    <div className='upload-form'>
+      <form>
+        <input type='file' onChange={changeHandler} />
+        <div className='output'>
+          {error && <div className='error'>{error}</div>}
+          {file && (
+            <div>
+              <img
+                style={{ width: '100px', height: '219px' }}
+                src={fileUrl}
+                alt='preview image'
+              />
+              <div>{file.name}</div>
+            </div>
+          )}
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default UploadForm;
